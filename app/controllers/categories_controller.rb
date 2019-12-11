7dc787f88edbb3660682f1ae306b5b07153e3845
vagrant <robert.beckett@onion.red>
2019-12-11 01:37:59 +0000

    Added authentication for destroy route of Categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  http_basic_authenticate_with name: Rails.configuration.admin[:admin_name], 
+                               password: Rails.configuration.admin[:admin_pass], only: :destroy
 
   def show
     @category = Category.find(params[:id])
