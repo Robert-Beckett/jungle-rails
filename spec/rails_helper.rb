03ff9546071e916e9a714cfa2e5bd076172a9cff
vagrant <robert.beckett@onion.red>
2019-12-17 01:59:02 +0000

    Completed home page test.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require "capybara/rails"
 require "capybara/rspec"
-require "capybara/poltegeist"
+require "capybara/poltergeist"
 
 # Specs flagged ith `js: true` will use Capybara's JS driver. Set
 # that JS driver to :poltergeist
@@ -47,7 +47,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  
+  ### config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
